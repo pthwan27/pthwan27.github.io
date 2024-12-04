d7b7c3be53409b5f4e79270f75ee3166d11746a7
pthwan27 <parkssuu@gmail.com>
2024-12-04 19:00:00 +0900

    build test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
-const repoName = "pthwan27.github.io";
+const repoName = "/";
 /**
  * @type {import('next').NextConfig}
  */
